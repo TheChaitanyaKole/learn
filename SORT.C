3df23f2c72a26dcf5c9adb7f99996c892d1abf2d
Chaitanya <chaitanyakole07@gmail.com>
2021-07-18 21:04:20 +0530

    Initial Commit

---
+++ SORT.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int arr[15],i,j ,temp,n;
+clrscr();
+printf("Enter no of Elements");
+scanf("%d",&n);
+for(i=0;i<n;i++){
+arr[i]=(rand()%10)+1;
+}
+for(i=0;i<n;i++){
+printf("%d\n",arr[i]);
+}
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+if(arr[j]>arr[j+1]){
+temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+}
+}
+printf("sorted array\n");
+for(i=0;i<n;i++){
+printf("%d\n",arr[i]);
+}
+getch();
+}
